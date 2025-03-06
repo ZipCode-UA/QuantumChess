29463839ba42e2880bab42d8ac43506d487f096a
Lucas Ergh <lucasergh@gmail.com>
2025-03-06 14:18:41 -0500

    Add include gurads and createMove method

--- include/quantumChess.h
+++ include/quantumChess.h
@@ -1,4 +1,7 @@
 
+#ifndef QUANTUMCHESS_H
+#define QUANTUMCHESS_H
+
 class QuantumChess{
 private:
 
@@ -7,4 +10,19 @@ public:
         Initializer for QuantumChess
     */
     QuantumChess();
+
+    // TODO Implement CreateMove
+    /*
+        Method to be called by window when user-input is gathered
+        If the move is valid it is played and the turn is switched
+
+        @param[in] startPos1
+        @param[in] endPos1
+        @param[in] startPos2
+        @param[in] endPos2
+        @return valid move
+    */
+    // bool CreateMove() {}
 };
+
+#endif
