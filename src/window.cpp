0ede2deec336fd46c5d180fb244c65ed5f66f77a
Lucas Ergh <LucasErgh@gmail.com>
2025-03-06 18:05:47 -0500

    Fix InitWindow call

--- src/window.cpp
+++ src/window.cpp
@@ -9,29 +9,29 @@
 #include "window.h"
 
 // @TODO implement constructor
-    /*
-        Initializer for window class
-    */
-    Window::Window(){
-        InitWindow(screenWidth, screenHeight);
-        Camera2D camera = { 0 };
-        camera.zoom = 1.0f;
+/*
+    Initializer for window class
+*/
+Window::Window(){
+    InitWindow(screenWidth, screenHeight, "Quantum Chess");
+    Camera2D camera = { 0 };
+    camera.zoom = 1.0f;
 
-        SetTargetFPS(30);
-    }
+    SetTargetFPS(30);
+}
 
-    // @TODO implement render method
-    /*
-        Render window
-    */
-    void Window::render(){
+// @TODO implement render method
+/*
+    Render window
+*/
+void Window::render(){
 
-    }
+}
 
-    // @TODO implement poll event method
-    /*
-        poll events for 
-    */
-    void Window::pollEvents(){
+// @TODO implement poll event method
+/*
+    poll events for 
+*/
+void Window::pollEvents(){
 
-    }
+}
