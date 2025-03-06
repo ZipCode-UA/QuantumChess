5da1b16431a1bbba1794d26ae868e91bf64a8d9a
Lucas Ergh <LucasErgh@gmail.com>
2025-03-06 17:30:06 -0500

    Fix CMakeLists lists for raylib and create Window class

---
+++ include/window.h
@@ -0,0 +1,21 @@
+/*
+    Header class
+*/
+
+#include "raylib.h"
+
+class Window{
+private:
+
+public:
+    /*
+        Initializer for window class
+    */
+    Window();
+
+    // @TODO implement render method
+    /*
+        Render window
+    */
+    void render();
+};
