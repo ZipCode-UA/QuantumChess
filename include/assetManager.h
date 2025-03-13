3b75c0087887844c9152d2ac4b48c3165d9ae713
Lucas Ergh <lucasergh@gmail.com>
2025-03-13 14:47:54 -0400

    Add empty methods and texture hashtable

--- include/assetManager.h
+++ include/assetManager.h
@@ -7,8 +7,30 @@
 #ifndef ASSETMANAGER_H
 #define ASSETMANAGER_H
 
-class AssetManager{
+#include "raylib.h"
+#include <unordered_map>
 
+class AssetManager{
+private:
+    std::unordered_map<int, Texture2D> pieces;
+
+public:
+    /*
+        Loads assets when constructed
+    */
+    AssetManager();
+
+    /*
+        Unload Assets when destructed
+    */
+    ~AssetManager();
+
+    /*
+        Retrieves a specific piece texture
+
+        @param[in] pieceKey key for the corresponding piece
+    */
+    Texture2D& retrievePiece(int pieceKey);
 };
 
 #endif
---
+++ src/assetManager.cpp
@@ -0,0 +1,30 @@
+/*
+    assetManager.h
+
+    Manages assets for the Quantum Chess application
+*/
+
+#include "assetManager.h"
+
+/*
+    Loads assets when constructed
+*/
+AssetManager::AssetManager(){
+
+}
+
+/*
+    Unload Assets when destructed
+*/
+AssetManager::~AssetManager(){
+
+}
+
+/*
+    Retrieves a specific piece texture
+
+    @param[in] pieceKey key for the corresponding piece
+*/
+Texture2D& AssetManager::retrievePiece(int pieceKey){
+
+}
