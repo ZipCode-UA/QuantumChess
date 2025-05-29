7d3b1faa469ede3d827f156f22792ededfb86f07
Elia Albaba <ea152@uakron.edu>
2025-05-28 21:54:01 -0400

    Add a list of moves for the queen

--- src/queen.cpp
+++ src/queen.cpp
@@ -7,10 +7,25 @@ Queen::Queen(SquareColor color, Pos position) : Piece(color, position) {
 }
 
 std::vector<std::pair<int, int>> Queen::getValidMoves(){
-    if(color == White){
-        return {{3, 0}};
+    std::vector<std::pair<int, int>> validMoves;
+
+    // Horizontal and vertical moves
+    for (int i = 1; i <= 7; ++i) {
+        validMoves.push_back({i, 0});      // Right
+        validMoves.push_back({-i, 0});     // Left
+        validMoves.push_back({0, i});      // Down
+        validMoves.push_back({0, -i});     // Up
     }
-    return {{-3, 0}};
+
+    // Diagonal moves
+    for (int i = 1; i <= 7; ++i) {
+        validMoves.push_back({i, i});      // Down-right
+        validMoves.push_back({i, -i});     // Down-left
+        validMoves.push_back({-i, i});     // Up-right
+        validMoves.push_back({-i, -i});    // Up-left
+    }
+
+    return {validMoves};
 }
 
 PieceID Queen::getType() const{
